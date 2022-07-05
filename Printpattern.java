3fdc49360e6261dcff303f68ea455ccb14dcd69d
Raj@9019 <rajbhushandixit99@gmail.com>
2022-07-05 21:24:01 +0530

    This commit is happening after resolving the index.lock problem

---
+++ Printpattern.java
@@ -0,0 +1,25 @@
+// In this java code we are just printing the following pattern.
+// *
+// **
+// *** 
+// **** 
+// ***** 
+ class pattern{
+    public static void main(String[] args){
+        //Method 1
+        System.out.println("This is one method:");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+
+        //Method 2
+        System.out.println("This is another method:");
+        System.out.print("*\n");
+        System.out.print("**\n");
+        System.out.print("***\n");
+        System.out.print("****\n");
+        System.out.print("*****\n");
+    }
+}

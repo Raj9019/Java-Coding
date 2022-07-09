cab280ae5ad3e1857e60d21976d5f4c6d2a80060
Raj@9019 <rajbhushandixit99@gmail.com>
2022-07-09 06:51:49 +0530

    Grater of two and three code is ready to run

---
+++ graterofthree.java
@@ -0,0 +1,16 @@
+class graterofthree{
+   public static void main(String args[]){
+      int a=0,b=10,c=2;
+      if(a>b){
+         if(a>c)
+            System.out.print(a+" is big");
+         else
+            System.out.print(c+" is big");
+      }
+      else if(b>c)
+        System.out.print(b+" is big");
+      else
+        System.out.print(c+" is big");
+  }
+}
+     
---
+++ grateroftwo.java
@@ -0,0 +1,11 @@
+class grateroftwo{
+   public static void main(String args[]){
+      int a,b;
+      a=2;
+      b=10;
+      if(a>b)
+        System.out.print(a+" is big");
+      else
+        System.out.print(b+" is big");
+  }
+}
